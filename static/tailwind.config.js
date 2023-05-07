460ce9df6fed41c9ed2de1ae1da1d883c1bce507
Kris Quigley <kris@krisquigley.co.uk>
2023-05-07 22:59:55 +0100

    chore: dx improvements

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss/types').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
-    '../templates/**/*.html.j2',
+    '../templates/**/*.html',
     './src/**/*.js',
     './node_modules/flowbite/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.violet,
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 };
